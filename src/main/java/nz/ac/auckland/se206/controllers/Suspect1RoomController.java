7784e47a208554eecbe535313e39289063f386c0
rtan654 <rtan654@aucklanduni.ac.nz>
2024-08-28 22:46:14 +1200

    Implementing all the logic for all the suspect rooms/scenes except the guessing buttons as might remove them for the suspect scenes.

--- src/main/java/nz/ac/auckland/se206/controllers/Suspect1RoomController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/Suspect1RoomController.java
@@ -1,28 +1,100 @@
 package nz.ac.auckland.se206.controllers;
 
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
 import nz.ac.auckland.se206.App;
+import nz.ac.auckland.se206.GameStateContext;
 
 public class Suspect1RoomController {
 
-  @FXML private Button btnRight;
+  @FXML private Button btnMenu;
+  @FXML private Button btnCrimeScene;
+  @FXML private Button btnGrandma;
+  @FXML private Button btnGrandson;
+  @FXML private Button btnUncle;
 
-  @FXML private AnchorPane rightAnchorPane;
+  private GameStateContext context = GameStateContext.getInstance();
 
+  /** Initializes the suspect 2 room view. */
   @FXML
-  public void initialize() {}
+  public void initialize() {
+    // Set initial visibility of the buttons
+    updateMenuVisibility();
+  }
 
   /**
-   * This method is called when the user clicks on the right button. It will take the user to the
-   * room screen.
+   * Handles the event when the crime scene button is clicked.
    *
-   * @throws IOException
+   * @throws IOException if the root cannot be set
    */
   @FXML
   private void onRoom() throws IOException {
     App.setRoot("room");
+  }
+
+  /**
+   * Handles the event when the grandson button is clicked.
+   *
+   * @param event the action event
+   * @throws IOException if the root cannot be set
+   */
+  @FXML
+  private void onGrandson(ActionEvent event) throws IOException {
+    App.setRoot("suspect3room");
+  }
+
+  /**
+   * Handles the event when the grandma button is clicked.
+   *
+   * @param event the action event
+   * @throws IOException if the root cannot be set
+   */
+  @FXML
+  private void onGrandma(ActionEvent event) throws IOException {
+    App.setRoot("suspect2room");
+  }
+
+  /**
+   * Toggles the visibility of the menu and other buttons when the menu button is clicked.
+   *
+   * @param event the action event
+   */
+  @FXML
+  private void onToggleMenu(ActionEvent event) {
+    context.toggleMenuVisibility(); // Toggle the visibility in the context
+    updateMenuVisibility(); // Update the visibility in the UI
+  }
+
+  /**
+   * Updates the visibility of the menu and other buttons based on the isMenuVisible variable in the
+   * GameStateContext.
+   */
+  private void updateMenuVisibility() {
+    boolean isMenuVisible = context.isMenuVisible();
+
+    if (isMenuVisible) {
+      btnMenu.setStyle(
+          "-fx-background-radius: 10 0 0 10; -fx-border-color:  black transparent black black;"
+              + " -fx-border-radius: 10 0 0 10; -fx-background-insets: 0;");
+    } else {
+      btnMenu.setStyle(
+          "-fx-background-radius: 20; -fx-border-radius: 20; -fx-border-color: black;"
+              + " -fx-background-insets: 0;");
+    }
+
+    // Set visibility and management of other buttons based on isMenuVisible
+    btnCrimeScene.setVisible(isMenuVisible);
+    btnCrimeScene.setManaged(isMenuVisible);
+
+    btnGrandma.setVisible(isMenuVisible);
+    btnGrandma.setManaged(isMenuVisible);
+
+    btnGrandson.setVisible(isMenuVisible);
+    btnGrandson.setManaged(isMenuVisible);
+
+    btnUncle.setVisible(isMenuVisible);
+    btnUncle.setManaged(isMenuVisible);
   }
 }
