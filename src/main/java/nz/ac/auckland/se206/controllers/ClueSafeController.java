bea6f4210a19fde804edb602a511659c3513d282
Conrad <cang688@aucklanduni.ac.nz>
2024-09-16 03:25:16 +1200

    created the functionality for the pin

--- src/main/java/nz/ac/auckland/se206/controllers/ClueSafeController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/ClueSafeController.java
@@ -1,15 +1,33 @@
 package nz.ac.auckland.se206.controllers;
 
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import nz.ac.auckland.se206.App;
+
 // import nz.ac.auckland.se206.GameStateContext;
 
 public class ClueSafeController {
 
   @FXML private AnchorPane anchorPane;
+  @FXML private Label codeDisplay;
+
+  private String line = "";
+
+  // @FXML private Button one;
+  // @FXML private Button two;
+  // @FXML private Button three;
+  // @FXML private Button four;
+  // @FXML private Button five;
+  // @FXML private Button six;
+  // @FXML private Button seven;
+  // @FXML private Button eight;
+  // @FXML private Button nine;
+  // @FXML private Button zero;
+  // @FXML private Button enter;
 
   @FXML
   private void initialize() {
@@ -26,7 +44,7 @@ public class ClueSafeController {
             + "-fx-border-insets: 0;");
     goBackButton.setPrefWidth(100);
     goBackButton.setPrefHeight(40);
-    
+
     anchorPane.getChildren().add(goBackButton);
 
     // Set the action when the button is clicked
@@ -40,4 +58,64 @@ public class ClueSafeController {
         });
   }
 
+  @FXML
+  private void onPin(ActionEvent event) {
+    Button clickedPin = (Button) event.getSource();
+    String pinId = clickedPin.getId();
+    if (line.length() < 3) {
+      switch (pinId) {
+        case "zero":
+          line = line + "0";
+          break;
+        case "one":
+          line = line + "1";
+          break;
+        case "two":
+          line = line + "2";
+          break;
+        case "three":
+          line = line + "3";
+          break;
+        case "four":
+          line = line + "4";
+          break;
+        case "five":
+          line = line + "5";
+          break;
+        case "six":
+          line = line + "6";
+          break;
+        case "seven":
+          line = line + "7";
+          break;
+        case "eight":
+          line = line + "8";
+          break;
+        case "nine":
+          line = line + "9";
+          break;
+        default:
+          // enter
+      }
+      codeDisplay.setText(line);
+    }
+
+    if (pinId.equals("delete")) {
+      line = "";
+      codeDisplay.setText(line);
+    }
+
+    if (pinId.equals("enter")) {
+      if (line.equals("019")) {
+        try {
+          App.setRoot("room");
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      } else {
+        line = "";
+        codeDisplay.setText("Incorrect");
+      }
+    }
+  }
 }
